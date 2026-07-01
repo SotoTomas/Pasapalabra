bfee7c7ce7ec264da916a6c45325a4b1624a4343
Tomas Soto <sototomas.nf@gmail.com>
2026-06-30 22:24:05 -0300

    .

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log("PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
